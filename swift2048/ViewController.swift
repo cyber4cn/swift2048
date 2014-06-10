5198f5c71b06f2cdbaf6dc71ba530b03dd77bfa5
wuxing <cyber4cn@gmail.com>
2014-06-10 15:59:25 +0800

    add Initial code

---
+++ swift2048/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift2048
+//
+//  Created by wuxing on 14-6-10.
+//  Copyright (c) 2014年 优才网（www.ucai.cn）. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
